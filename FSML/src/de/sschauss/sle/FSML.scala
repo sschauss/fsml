1217bbd4ee08f2757f2d43fb2154f1cfaaaa11ee
Simon Schauß <s.schauss@bitzeche.de>
2013-12-03 17:33:15 +0100

    started checks

--- FSML/src/de/sschauss/sle/FSML.scala
+++ FSML/src/de/sschauss/sle/FSML.scala
@@ -7,30 +7,55 @@ import scala.util.parsing.input.CharArrayReader
 import scala.io.Source
 import java.io.File
 
-object FSMLParser extends JavaTokenParsers {
+case class Fsm(val states: List[State])
 
-  def fsm: Parser[Any] = rep(state)
+case class State(val initial: Boolean, val id: String, val transitions: List[Transition])
 
-  def state: Parser[Any] = initial ~ "state" ~ id ~ "{" ~ rep(transition) ~ "}"
+case class Transition(val input: String, val action: Option[String], val id: Option[String])
 
-  def initial: Parser[Any] = "initial" | ""
+object FsmlParser extends JavaTokenParsers {
 
-  def transition: Parser[Any] = input ~ opt("/" ~ action) ~ opt("->" ~ id) ~ ";"
+  def fsm: Parser[Fsm] = rep(state) ^^ {
+    case states => Fsm(states)
+  }
 
-  def id: Parser[Any] = "[A-Za-z0-9]*".r
+  def state: Parser[State] = initial ~ "state" ~ id ~ "{" ~ rep(transition) ~ "}" ^^ {
+    case initial ~ _ ~ id ~ _ ~ transitions ~ _ => State(initial, id, transitions)
+  }
 
-  def input: Parser[Any] = "[A-Za-z0-9]*".r
+  def initial: Parser[Boolean] = "initial" ^^ { _ => true } | "" ^^ { _ => false }
 
-  def action: Parser[Any] = "[A-Za-z0-9]*".r
+  def transition: Parser[Transition] = input ~ opt("/" ~ action) ~ opt("->" ~ id) ~ ";" ^^ {
+    case input ~ None ~ None ~ _ => Transition(input, None, None)
+    case input ~ Some(_ ~ action) ~ None ~ _ => Transition(input, Some(action), Some(input))
+    case input ~ None ~ Some(_ ~ id) ~ _ => Transition(input, None, Some(id))
+    case input ~ Some(_ ~ action) ~ Some(_ ~ id) ~ _ => Transition(input, Some(action), Some(id))
+  }
+
+  def id: Parser[String] = "[A-Za-z0-9]*".r
+
+  def input: Parser[String] = "[A-Za-z0-9]*".r
+
+  def action: Parser[String] = "[A-Za-z0-9]*".r
+
+}
+
+object FsmChecker {
+
+  def okFsm(fsm: Fsm): Boolean =
+    fsmSingleInitial(fsm)
+
+  def fsmSingleInitial(fsm: Fsm): Boolean =
+    fsm.states.filter(state => state.initial).length == 1
 
 }
 
 object Runner {
 
   def main(args: Array[String]) {
-    val fsmlParser = FSMLParser
+    val fsmlParser = FsmlParser
     val input = Source.fromFile(args(0)).mkString
-    println(fsmlParser.parseAll(fsmlParser.fsm, input))
+    val output = fsmlParser.parseAll(fsmlParser.fsm, input)
   }
 
 }
